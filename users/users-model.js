38c2323455fd535003327b9b29a37466c6fa8116
Justin Brandley <brandleyj@gmail.com>
2019-11-13 15:53:26 -0600

    added user-router and user model

---
+++ users/users-model.js
@@ -0,0 +1,27 @@
+const db = require("../data/dbConfig");
+
+module.exports = {
+	add,
+	find,
+	findBy,
+	findById
+};
+
+function find() {
+	return db("users").select("id", "username", "password", "department");
+}
+
+function findBy(filter) {
+	return db("users").where(filter);
+}
+
+function findById(id) {
+	return db("users")
+		.where({ id })
+		.first();
+}
+
+async function add(user) {
+	const newUser = await db("users").insert(user);
+	console.log(newUser);
+}
